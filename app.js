57f55a3fa83d724cdc43f241c2f68944f8f22050
AlanGMerkin <alangmerkin@gmail.com>
2021-05-31 23:29:26 -0300

    AlanV2

--- app.js
+++ app.js
@@ -3,12 +3,20 @@ const app = express();
 const path = require("path");
 
 app.use('/public/img', express.static(__dirname +'/public/img'));
-app.listen(3000, () =>
+app.use('/public/css/estilos_ingreso.css', express.static(__dirname +'/public/css/estilos_ingreso.css'));
+app.use('/public/css/estilos_registro.css', express.static(__dirname +'/public/css/estilos_registro.css'));
+app.listen(3030, () =>
     console.log("Levantando el servidor")
     );
 
 
-app.get("/", (req, res) => {
+app.get("/registro", (req, res) => {
     let htmlPath= path.join(__dirname, "/views/registro.html")
     res.sendFile(htmlPath)
 });
+
+app.get("/ingreso", (req, res) => {
+    let htmlPath= path.join(__dirname, "/views/ingreso.html")
+    res.sendFile(htmlPath)
+});
+
