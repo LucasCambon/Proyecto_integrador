33e900e0d274ea78044e16423708721cc82d7bf5
LucasCambon <Lucambon08@gmail.com>
2021-05-31 23:51:26 -0300

    print

--- app.js
+++ app.js
@@ -1,16 +1,15 @@
 const express = require("express");
 const app = express();
-const path = require("path");
+const path = require('path');
 
 app.use('/public/img', express.static(__dirname +'/public/img'));
+app.use('/public/css', express.static(__dirname + '/public/css'));
 app.listen(3000, () =>
     console.log("Levantando el servidor")
-    );      
-app.use('/public/css/stylesDetalleDeProducto.css',express.static(__dirname + '/public/css/stylesDetalleDeProducto.css'));
+    );
 
 
 app.get("/", (req, res) => {
-    let htmlPath= path.join(__dirname, "/views/detalleDeProducto.html")
+    let htmlPath= path.join(__dirname, "/views/index.html")
     res.sendFile(htmlPath)
 });
-
