1973a2860473fc5e99a59cb855119fb3e6bc469b
LucasCambon <Lucambon08@gmail.com>
2021-07-22 01:16:44 -0300

    Login session primera parte

--- src/routes/usersRoutes.js
+++ src/routes/usersRoutes.js
@@ -5,17 +5,17 @@ const usersController = require("./../controllers/usersController")
 
 const validations = require("../middlewares/validationRegisterUser")
 
-// REGISTRO //
+/*** REGISTRO ***/
 router.get("/registro",usersController.registro)
 router.post("/registro",validations,usersController.store)
 
-// INGRESO //
+/*** INGRESO ***/
 router.get("/ingreso",usersController.ingreso)
 router.post("/ingreso",usersController.login)
 
 
-// PERFIL //
-router.get("/perfil",usersController.perfil)
+/*** PERFIL***/
+router.get("/perfil",usersController.profile)
 
 
 module.exports = router;
