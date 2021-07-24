b9a319440651a5dd11fea5d63fbcc28ebfd60bed
LucasCambon <Lucambon08@gmail.com>
2021-07-24 19:17:05 -0300

    Fixes de validacion de imagenes

--- src/middlewares/multerMiddleware.js
+++ src/middlewares/multerMiddleware.js
@@ -1,5 +1,21 @@
 const path = require('path');
 const multer = require("multer")
+const { validationResult } = require("express-validator")
+
+let acceptedExtensions = [".jpg", ".png", ".gif"]
+
+const fileFilter = function (req, file, cb){
+    let fileExtension = path.extname(file.originalname);
+        if (!acceptedExtensions.includes(fileExtension)){
+            return cb(null, false);
+        }
+        else{
+           return cb(null, true);
+            
+        }
+        
+} 
+
 const multerDiskStorage = multer.diskStorage({
     destination: function(req, file, cb) {       // request, archivo y callback que almacena archivo en destino
      cb(null, path.join(__dirname,"../../public/img"));    // Ruta donde almacenamos el archivo
@@ -10,6 +26,8 @@ const multerDiskStorage = multer.diskStorage({
     }
 });
 
-const uploadFile = multer({ storage: multerDiskStorage });
+const uploadFile = multer({ 
+    fileFilter : fileFilter,
+    storage: multerDiskStorage });
 
 module.exports = uploadFile;
