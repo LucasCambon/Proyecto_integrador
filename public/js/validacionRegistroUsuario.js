d6f744d6179c320a0a87cb905ba6c548ff8697a8
LucasCambon <Lucambon08@gmail.com>
2021-09-13 02:02:07 -0300

    Validation imagenes (FRONT) & validation registro productos

--- public/js/validacionRegistroUsuario.js
+++ public/js/validacionRegistroUsuario.js
@@ -34,7 +34,7 @@ function checkInputs() {
     let apellido = document.getElementById("apellidos")
     let email = document.getElementById("correo")
     let contraseña = document.getElementById("contraseña")
-
+    let imagen = document.getElementById("imagenPerfil")
 
     /// CARACTERES, NÚMEROS, MAYÚSCULAS Y MINÚSCULAS
     let obligatorio =  /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[^a-zA-Z0-9])(?!.*\s).{8,15}$/;
@@ -43,7 +43,7 @@ function checkInputs() {
     /// NOMBRE
     let nombreValue = nombre.value
     
-    if (nombreValue == "" || nombreValue.match(noPermitido) || /^\d+$/.test(nombreValue)) {
+    if (nombreValue == "" || nombreValue.match(noPermitido) || /^\d+$/.test(nombreValue) || nombreValue.length > 50) {
         setError(nombre, "Tiene que ingresar su nombre, no puede contener números o caracteres especiales")
     }
     else{
@@ -53,7 +53,7 @@ function checkInputs() {
 
     /// APELLIDO
     let apellidoValue = apellido.value.trim()
-    if (apellidoValue === "" || apellidoValue.match(noPermitido) || /^\d+$/.test(apellidoValue)) {
+    if (apellidoValue === "" || apellidoValue.match(noPermitido) || /^\d+$/.test(apellidoValue) || apellidoValue.length > 50) {
         setError(apellido, "Tiene que ingresar su apellido, no puede contener números o caracteres especiales")
     }
     else{
@@ -65,7 +65,7 @@ function checkInputs() {
     let emailValue = email.value.trim()
     let isEmail = /\S+@\S+\.\S+/.test(emailValue)
 
-    if (emailValue != "" && isEmail) {
+    if (emailValue != "" && isEmail && emailValue.length < 150) {
         setSucces(email)
     }
     else{
@@ -82,6 +82,23 @@ function checkInputs() {
     else{
         setError(contraseña, "La contraseña debe tener mínimo 8 caracteres y contener una mayúscula, una minúscula y un caracter especial")
     }
+
+
+    /// IMAGEN
+    let imagenValue = imagen.value.trim()
+    let validExtensions = ['jpg','png','jpeg','gif'];
+    let extImagen = imagenValue.split(".").pop()
+
+    console.log(extImagen)
+    for (let i = 0; i <= validExtensions.length; i++){
+        if (imagenValue !== "" && validExtensions.includes(extImagen) ) {
+            setSucces(imagen)
+        }
+        else{
+            setError(imagen, "Adjunte una imagen con formato: " + validExtensions + " y peso máximo 10mb.")
+        }
+    }
+
 }
 
 function setError(input, mensaje) {
